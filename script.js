4ba72ff9a7e41be50ae9825bea9d9bc52bd5fc23
dim2019 <dmitriy.mezvrishvili@mail.ru>
2021-07-17 23:18:06 +0400

    add dynamic JS/CSS

---
+++ script.js
@@ -0,0 +1,39 @@
+var prev = document.getElementById("prev")
+var next = document.getElementById("next")
+var thumbnail = document.getElementsByClassName("thumbnail")
+var hero = document.getElementById("hero")
+var ProductImg = document.getElementById("ProductImg");
+
+var backgroundImg = new Array(
+    "IMG/Tbilisi.jpg",
+    "IMG/gori (2).jpg",
+    "IMG/kaxeti.jpg",
+    "IMG/batumi.jpg",
+    "IMG/martvili.jpg",
+
+);
+let i = 0;
+next.onclick = function() {
+    if (i < 4) {
+        hero.style.backgroundImage = 'url("' + backgroundImg[i + 1] + '")'
+        thumbnail[i + 1].classList.add("active")
+        thumbnail[i].classList.remove("active")
+
+        i++;
+    }
+}
+prev.onclick = function() {
+
+    if (i > 0) {
+        hero.style.backgroundImage = 'url("' + backgroundImg[i - 1] + '")'
+        thumbnail[i - 1].classList.add("active")
+        thumbnail[i].classList.remove("active")
+        i--;
+
+    }
+}
+thumbnail[0].onclick = function() {
+    hero.style.backgroundImage = backgroundImg[0]
+
+    i++
+}
